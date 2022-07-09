1660129d7d78ce3df5ce23b0a97c14eb5ee7bac8
Vaibhav Garg <67408018+vaibhavgarg237@users.noreply.github.com>
2022-07-09 18:21:33 +0530

    28- call and autopopulate from api next prev

--- src/components/News.js
+++ src/components/News.js
@@ -2,34 +2,97 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export default class News extends Component {
+  constructor() {
+    super();
+    // console.log("helllo");
+    this.state = {
+      articles: [],
+      loading: true,
+      page: 1,
+    };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=ce45ade3accc4c6a913c4113ea1b07e3&pagesize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData.articles);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handlePrevClick = async () => {
+    console.log("!");
+    // if (this.state.page - 1 >= 1) {
+    console.log("!!");
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=ce45ade3accc4c6a913c4113ea1b07e3&page=${
+      this.state.page - 1
+    }&pagesize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData.articles);
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1,
+    });
+    // }
+  };
+
+  handleNextClick = async () => {
+    // console.log("without cond");
+    // console.log(this.state.page + 1);
+    // console.log(this.state.totalResults);
+    if (this.state.page + 1 <= Math.ceil(this.state.totalResults / 20)) {
+      // console.log("clicked next");
+      let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=ce45ade3accc4c6a913c4113ea1b07e3&page=${
+        this.state.page + 1
+      }&pagesize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData.articles);
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page + 1,
+      });
+    }
+  };
+
   render() {
     return (
       <div className="container my-3">
         <h2>NewsMonkey</h2>
         <div className="row">
-          <div className="col col-md-4">
-            <NewsItem title="Vaibhav" description="sfw eng" />
-          </div>
-          <div className="col col-md-4">
-            <NewsItem title="Vaibhav" description="sfw eng" />
-          </div>
-          <div className="col col-md-4">
-            <NewsItem title="Vaibhav" description="sfw eng" />
-          </div>
+          {this.state.articles.map((el) => {
+            return (
+              <div className="col col-md-4" key={el.publishedAt}>
+                <NewsItem
+                  title={el.title}
+                  description={el.description}
+                  imgURL={el.urlToImage}
+                />
+              </div>
+            );
+          })}
         </div>
-        <div className="row">
-          <div className="col col-md-4">
-            <NewsItem title="Vaibhav" description="sfw eng" />
-          </div>
-          <div className="col col-md-4">
-            <NewsItem title="Vaibhav" description="sfw eng" />
-          </div>
-          <div className="col col-md-4">
-            <NewsItem title="Vaibhav" description="sfw eng" />
-          </div>
-          <div className="col col-md-4">
-            <NewsItem title="Vaibhav" description="sfw eng" />
-          </div>
+        <div className=" d-flex justify-content-between">
+          <button
+            type="button"
+            disabled={this.state.page < 1}
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previus
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imgURL } = this.props;
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
+          <img src={imgURL} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/#" className="btn btn-sm btn-primary">
+            <a href={imgURL} className="btn btn-sm btn-primary">
               Read More
             </a>
           </div>
